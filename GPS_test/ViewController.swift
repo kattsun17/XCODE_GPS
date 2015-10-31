9515018132712837d25aa8ccd74e01f97f356ede
kattsun <s14t222@stmail.eng.kagawa-u.ac.jp>
2015-10-31 18:05:46 +0900

    Initial Commit

---
+++ GPS_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPS_test
+//
+//  Created by Gakusei Katsumura on 10/31/15.
+//  Copyright © 2015 gakusei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
